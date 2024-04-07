8e58b233c4bdd025956fa74600be0fcd11eaab7e
Sergey Zabyrnykov <zabyrnykov.s@soft2bet.com>
2024-04-08 00:20:51 +0300

    Commit

--- backend2/controllers/UserController.js
+++ backend2/controllers/UserController.js
@@ -1,7 +1,7 @@
 const ApiError = require("../error/ApiError");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { User } = require("../models/User");
+const { User } = require("../models");
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
