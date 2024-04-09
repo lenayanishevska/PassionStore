25395ae6998f490aa10d3554bcb29c59d1103e2d
Olena Yanishevska <olena.yanishevska.pz.2021@lpnu.ua>
2024-04-09 20:22:58 +0300

    Add Product Detail Page

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -6,7 +6,7 @@ import product from '../../assets/pictures/Berfinben.jpeg'
 export default function Product({item}) {
   return (
     <div className='product flex-column'>
-        <img src='' alt="" />
+        <img src={item.img[0]} alt="" />
         <div className="info flex-column">
             <div className="name-like flex-row">
                 <h3>{item.name}</h3>
